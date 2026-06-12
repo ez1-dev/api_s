9d770450bfe2d59f7f75deb99a790de6bd318668
Pedro <pedrohfagundes2002@gmail.com>
2026-06-12 11:39:07 -0300

    Remove hardcoded password from database configuration for on-prem and cloud setups

--- api_sapiens_control_center.py
+++ api_sapiens_control_center.py
@@ -68,7 +68,6 @@ _ONPREM_DB_DEFAULTS = {
     "port":     "1433",
     "database": "sapiens",
     "user":     "sapiens",
-    "password": "0n%lV'g0F94",
     "trust":    "yes",
 }
 _CLOUD_DB_DEFAULTS = {
@@ -77,7 +76,6 @@ _CLOUD_DB_DEFAULTS = {
     "port":     "1433",
     "database": "",
     "user":     "",
-    "password": "0n%lV'g0F94",
     "trust":    "yes",
 }
 EMPRESA_PADRAO = 1
